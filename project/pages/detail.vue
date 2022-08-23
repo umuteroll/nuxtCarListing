68fedd88019d7d5c7451eae1c903cff9d2b8c516
Umut Erol <umut_erol1999@hotmail.com>
2022-08-23 23:14:25 +0300

    feat: project created with nuxt

---
+++ project/pages/detail.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="container">
+    <NuxtLink to="/detail"></NuxtLink>
+detail page 
+</div>
+   
+</template>
---
+++ project/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="container">
+  <NuxtLink to="/"></NuxtLink>
+home page
+</div>
+
+</template>
